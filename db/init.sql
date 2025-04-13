9b41c90b2bec314e1b9ad7f1584cada6a011a129
Bablu Alam <bablu030397@gmail.com>
2025-04-13 18:46:05 +0530

    final commit

--- db/init.sql
+++ db/init.sql
@@ -1,4 +1,4 @@
-CREATE DATABASE sampledb;
+CREATE DATABASE IF NOT EXISTS sampledb;
 USE sampledb;
 CREATE TABLE users (id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(255));
 INSERT INTO users (name) VALUES ('Bablu'), ('Docker'), ('DevOps');
